e2b0bc4b19262c9f433f97cb8cc970f28d878dd0
Your Name <you@example.com>
2021-05-09 21:43:16 +0530

    NG-230 - First Time File Updated

---
+++ src/main/java/com/springboot/runner/MessageRunner.java
@@ -0,0 +1,14 @@
+package com.springboot.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+      System.out.println("Well come Spriingboot App");
+	}
+
+}
